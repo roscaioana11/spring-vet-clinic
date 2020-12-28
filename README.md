85c7974b6e8474c3013fac2cec046f80cbc603bc
rosca_ioana <rosca.ioana11@gmail.com>
2020-12-28 21:58:08 +0200

    Beginning the app creation

--- README.md
+++
@@ -1,1 +0,0 @@
-# spring-vet-clinic
---
+++ src/test/java/ro/fasttrackit/vetclinic/VetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.fasttrackit.vetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
